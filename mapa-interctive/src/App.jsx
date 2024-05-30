d0be9e054708fb9a7f54b8e2a459e6e5dfecbdb4
kevinCapera14 <kevincapera0105@gmail.com>
2024-05-30 18:06:56 -0500

    start app

---
+++ mapa-interctive/src/App.jsx
@@ -0,0 +1,11 @@
+import Mapa from "./components/Mapa"
+import Header from "./components/Header"
+
+const App = ()=>{
+    return <div className="">
+        <Header/>
+        <Mapa/>
+    </div>
+}
+
+export default App
